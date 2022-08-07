f261986640d6a71014e2a0c34441896f8ce74598
Hitesh <hites_297@yahoo.co.in>
2022-08-06 20:37:12 -0400

    fontawesome icons seeded

--- database/factories/SkillFactory.php
+++ database/factories/SkillFactory.php
@@ -20,7 +20,16 @@ class SkillFactory extends Factory
             'type' => $this->faker->randomElement(['Framework','Tools','Database','Javascript','Testing','Devops']),
             'details' => $this->faker->randomElement(['C#, PHP, javascript, HTML, CSS','jenkins, jfrog, shellscript','SQL, DB2, MYSQL, Mongo','.NET, Laravel, React, Vue']),
             'sequence' => $this->faker->randomDigit(),
-            'fontawesomeHTML' => $this->faker->word(),
+            'fontawesomeHTML' => $this->faker->randomElement(['<i class="fa-solid fa-beer-mug-empty"></i>',
+            '<i class="fa-solid fa-bomb"></i>',
+            '<i class="fa-solid fa-broom-ball"></i>',
+            '<i class="fa-solid fa-dragon"></i>',
+            '<i class="fa-solid fa-floppy-disk"></i>',
+            '<i class="fa-solid fa-motorcycle"></i>',
+            '<i class="fa-solid fa-keyboard"></i>',
+            '<i class="fa-solid fa-network-wired"></i>',
+            '<i class="fa-solid fa-share-nodes"></i>',
+            '<i class="fa-solid fa-camera-retro"></i>']),
         ];
     }
 }
--- database/factories/SocialmediaFactory.php
+++ database/factories/SocialmediaFactory.php
@@ -17,7 +17,16 @@ class SocialmediaFactory extends Factory
     public function definition()
     {
         return [
-            'logo' => $this->faker->word(),
+            'logo' => $this->faker->randomElement(['<i class="fa-solid fa-beer-mug-empty"></i>',
+            '<i class="fa-solid fa-bomb"></i>',
+            '<i class="fa-solid fa-broom-ball"></i>',
+            '<i class="fa-solid fa-dragon"></i>',
+            '<i class="fa-solid fa-floppy-disk"></i>',
+            '<i class="fa-solid fa-motorcycle"></i>',
+            '<i class="fa-solid fa-keyboard"></i>',
+            '<i class="fa-solid fa-network-wired"></i>',
+            '<i class="fa-solid fa-share-nodes"></i>',
+            '<i class="fa-solid fa-camera-retro"></i>']),
             'url' => $this->faker->unique()->url(),
             'sequence' => $this->faker->randomDigit(),
         ];
