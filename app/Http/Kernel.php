ee71071acff701e7cc72aea1fb4c921bb207d1eb
Hitesh <hites_297@yahoo.co.in>
2022-08-11 12:22:59 -0400

    added cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,6 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        'cors' => \App\Http\Middleware\Cors::class
     ];
 
     /**
@@ -64,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
