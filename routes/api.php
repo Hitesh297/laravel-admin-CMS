99cb154efa0defd579b98774cc96c7c141280a50
Hitesh <hites_297@yahoo.co.in>
2022-08-15 01:54:21 -0400

    undo previous commit

--- routes/api.php
+++ routes/api.php
@@ -10,10 +10,6 @@ use App\Models\Skill;
 use App\Models\Socialmedia;
 use App\Models\User;
 
-
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 /*
 |--------------------------------------------------------------------------
 | API Routes
