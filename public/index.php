8bb6cf950378191321f65a02549300e45f003235
Hitesh <hites_297@yahoo.co.in>
2022-12-12 22:32:56 -0500

    allow all origin

--- public/index.php
+++ public/index.php
@@ -53,6 +53,3 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
-
-header('Access-Control-Allow-Origin: *');  
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
