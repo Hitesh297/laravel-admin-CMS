9815b6bb8c9b72945f4de3c42b732ff38ff78240
Hitesh <hites_297@yahoo.co.in>
2022-12-12 22:37:05 -0500

    new cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -4,20 +4,36 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-// class Cors
-// {
-//     /**
-//      * Handle an incoming request.
-//      *
-//      * @param  \Illuminate\Http\Request  $request
-//      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-//      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-//      */
-//     public function handle(Request $request, Closure $next)
-//     {
-//         return $next($request)
-//             ->header('Access-Control-Allow-Origin', '*')
-//             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     }
-// }
+class CORS {
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
+        ];
+        if($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
+    }
+
+}
